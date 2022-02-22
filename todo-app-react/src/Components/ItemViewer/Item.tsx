442dddb768d159952f937517eb92346895fe4269
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-02-23 11:52:58 +1300

    Update Label

--- todo-app-react/src/Components/ItemViewer/Item.tsx
+++ todo-app-react/src/Components/ItemViewer/Item.tsx
@@ -200,10 +200,10 @@ function Item(props: ItemProps)
                         Delete
                     </button>
                     <span>
-                        Date Created: {new Date(props.dateCreated).toLocaleDateString('en-NZ')}
+                        Created: {new Date(props.dateCreated).toLocaleDateString('en-NZ')}
                     </span>
                     <span>
-                        Date Modified: {new Date(props.dateModified).toLocaleDateString('en-NZ')}
+                        Modified: {new Date(props.dateModified).toLocaleDateString('en-NZ')}
                     </span>
                 </div>
             </div>
